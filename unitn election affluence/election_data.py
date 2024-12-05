a30d8ff70ebc00a4bee385920d27e43f97ab79de
sarasavani <sarasavani03@gmail.com>
2024-12-05 11:50:58 +0100

    creating new file
    lap_times.py

--- unitn election affluence/election_data.py
+++ unitn election affluence/election_data.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 data= pd.read_csv("Tabella_Affluenza_Comma.csv")
 #print(ds)
 
-plt.rcParams["figure.figsize"] = (10,5)
+plt.rcParams["figure.figsize"] = (10,5) #size of the window set to 10x5
 
-df=pd.DataFrame(data)
-groups = ['1996','1998','2000','2002','2004','2006','2008', '2010','2012','2014','2016','2018', '2020','2022','2024']
+df=pd.DataFrame(data) #creating a dataframe with the data read above
+groups = list(df.iloc[:,0])
+# ['1996','1998','2000','2002','2004','2006','2008', '2010','2012','2014','2016','2018', '2020','2022','2024']
 
 #15 values
-X= list(df.iloc[:,1])
+X= list(df.iloc[:,1]) #the values on the X-axis are the values on the column one (aventi diritto)
 X.reverse()
 Y= list(df.iloc[:,2])
 Y.reverse()
